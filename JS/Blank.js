5eccfce22cacef5eaf9175a614ff78150def92da
Théo <tochampion38@gmail.com>
2019-07-12 10:11:34 +0200

    import local repo to github

---
+++ JS/Blank.js
@@ -0,0 +1,26 @@
+// ─── IMPORT ALL DEPENDENCIES FOR THIS CLASS ─────────────────────────────────────
+
+export default class ClassName {
+
+    /*📝 Summary of all functions
+    ...
+    */
+
+    constructor() {
+        this.initClass();
+        this.initElems();
+    }
+
+    // ─── INIT MAIN METHODS OF THE CLASS ─────────────────────────────────────────────
+    initClass() {
+
+    }
+
+    // ─── DECLARATION OF ALL DOM ELEMENTS SET IN VARIABLES ───────────────────────────
+    initElems() {
+        //Jquery elements
+        $el = {
+
+        }
+    }
+}
